c48b0d5abd804028326b8a29279381fe1f597017
10hin <10hin@outlook.jp>
2025-12-27 02:48:05 +0900

    update

--- mydag1.py
+++ mydag1.py
@@ -7,7 +7,8 @@ from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 with DAG('mydag1') as dag:
     S3CreateObjectOperator(
         task_id='create_s3_object',
-        bucket_name='my-bucket',
+        s3_bucket='my-bucket',
         s3_key='{{ ds }}_{{ run_id }}.txt',
         data='This is the content of the file.',
+        replace=True,
     )
